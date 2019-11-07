426c31e14abfdff3c8ea6aa709fa187595a96833
Renato Santos <renato@midilabs.com.br>
2019-11-07 12:15:08 -0200

    Atualizando códigos.

--- src/main/java/com/midilabs/dentech/api/repository/ProcessoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.midilabs.dentech.api.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.midilabs.dentech.api.model.vo.Processo;
-
-public interface ProcessoRepository extends JpaRepository<Processo, Long> {
-
-}
